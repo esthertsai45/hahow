c0b4f082983e3e18ed9991c4961189db1477e32f
Esther Tsai <esther.tsai@appier.com>
2025-10-05 13:11:45 +0800

    add Paginator class to fetch all API response data

---
+++ lib/util.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+class Paginator:
+    """
+    handle the pagination of the API
+    """
+    @staticmethod
+    def fetch_all(url: str) -> list:        
+        all_results = []
+        current_url = url
+
+        while current_url:
+            try:
+                response = requests.get(current_url)
+                response.raise_for_status()
+                data = response.json()
+                
+                if isinstance(data, dict):
+                    if 'results' in data:
+                        all_results.extend(data['results'])
+                        current_url = data.get('next')
+                    else:
+                        all_results.append(data)
+                        current_url = None  
+                elif isinstance(data, list):
+                    all_results.extend(data)
+                    current_url = None 
+                else:
+                    all_results.append(data)
+                    current_url = None
+                    
+            except requests.exceptions.RequestException as e:
+                print(f"API request error: {e}")
+                break
+        return all_results
